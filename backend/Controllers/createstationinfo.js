60ee0707553d8b7caaafb4fc68b201678e713bd4
Sarthak vyas <sarthakmanju2104@gmail.com>
2024-06-01 20:47:35 +0530

    commtiting a new day

--- backend/Controllers/createstationinfo.js
+++ backend/Controllers/createstationinfo.js
@@ -2,10 +2,11 @@ const Station = require("../Models/station");
 
 exports.createstationinfo = async (req, res) => {
     try {
-        const { name, arrival, departure } = req.body;
+        const { name } = req.body;
         const newstation = new Station({
             name: name,
         });
+        console.log("new station = "+newstation)
         const stationinfo = await newstation.save();
         res.json({
             message: "Successfully created station info",
